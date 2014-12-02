70abb90980fa41641199115510e73a69a6f16489
Liz Portnoy <lizport10@gmail.com>
2014-12-01 22:38:38 -0800

    basic functionality built

--- client/app/display/display.js
+++ client/app/display/display.js
@@ -2,29 +2,47 @@ angular.module('mapYourMeal.display', [])
 
 .controller('DisplayController', function ($scope, RecipeStorage) {
 
-
-  $scope.displayAll = function () {
-    $scope.recipeName = RecipeStorage.displayedRecipe.recipeName
-    $scope.recipeSteps = RecipeStorage.displayedRecipe.steps;
-    $scope.mealTime = RecipeStorage.displayedRecipe.mealTime;
-  };
-
-  $scope.sumTimes = RecipeStorage.displayedRecipe.steps.reduce(function (accumulator, twople) {
-    return accumulator + twople[1];
+  
+  $scope.recipeName = RecipeStorage.displayedRecipe.recipeName
+  $scope.recipeSteps = RecipeStorage.displayedRecipe.steps;
+  $scope.mealTime = RecipeStorage.displayedRecipe.mealTime;
+  $scope.timeRemaining = RecipeStorage.displayedRecipe.steps.reduce(function (accumulator, recipeStep) {
+    return accumulator + recipeStep[1];
   },0);
 
-  $scope.displayAll();
-  $scope.displayedTime = $scope.mealTime;
-  $scope.timeLeft = $scope.sumTimes
-
-
-
-  $scope.displayedTime = moment({hours:$scope.displayedTime.toString().slice().split(':')[0], minutes: $scope.displayedTime.toString().slice().split(':')[1]})
-    .subtract($scope.timeLeft, 'minutes')
-    .format('h:mm a');
-
-  $scope.formatTime = function (time) {
-    $scope.timeLeft -= time; 
+  $scope.addTimeCounters = function () {
+    _.each(RecipeStorage.displayedRecipe.steps, function (elem) {
+       elem.push($scope.timeRemaining);
+      $scope.timeRemaining  = $scope.timeRemaining - elem[1];
+  });
   }
+
+
+  $scope.addTimeCounters();
+
+  $scope.displayedTime;
+
+  
+  $scope.formatTime = function (mealTime, timeLeft) {
+    
+    // converts hours to readable format by 
+    var hours = mealTime.toString().slice().split(':')[0];
+    // converts hours to redeable forma
+
+    var minutes = mealTime.toString().slice().split(':')[1].substring(0,2);
+
+    console.log('hours', hours);
+    console.log('minutes', minutes);  
+
+
+    $scope.displayedTime = moment({hours: hours, minutes: minutes})
+      .subtract(timeLeft, 'minutes')
+      .format('h:mm a');
+
+    return $scope.displayedTime;
+
+  }
+
+
 
 });
